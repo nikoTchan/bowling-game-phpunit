b0e350b763c58d194a9d66ccdaf2c78a556cd139
Raúl Araya <nubeiro@gmail.com>
2016-05-05 23:07:18 +0200

    Test gameAllOnes.

--- src/Game.php
+++ src/Game.php
@@ -4,10 +4,22 @@ namespace Bowling;
 class Game
 {
 
+    private $score;
+
     /**
      * Game constructor.
      */
     public function __construct()
     {
+    }
+
+    public function roll($int)
+    {
+        $this->score += $int;
+    }
+
+    public function score()
+    {
+        return $this->score;
     }
 }
--- tests/GameTest.php
+++ tests/GameTest.php
@@ -5,8 +5,37 @@ namespace Bowling;
 class GameTest extends \PHPUnit_Framework_TestCase
 {
 
+    private $game;
+
     public function testGame()
     {
-        new Game();
+        $this->rollMany(20, 0);
+        $this->assertSame(0, $this->game->score());
+    }
+
+    public function testGameAllOnes()
+    {
+
+        $this->rollMany(20, 1);
+        $this->assertSame(20, $this->game->score());
+    }
+
+    /**
+     * @return Game
+     */
+    public function setUp()
+    {
+        $this->game = new Game();
+    }
+
+    /**
+     * @param $n
+     * @param $pins
+     */
+    protected function rollMany($n, $pins)
+    {
+        for ($i = 0; $i < $n; $i++) {
+            $this->game->roll($pins);
+        }
     }
 }
