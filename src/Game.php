b0db3addbdeffc767b35c6e9c69d80d79fc0036a
Marc Nicolàs i Aragó <marc.niko@gmail.com>
2016-05-12 23:28:26 +0200

    Third, fourth and fifth test, and refactoring.

--- src/Game.php
+++ src/Game.php
@@ -3,8 +3,10 @@ namespace Bowling;
 
 class Game
 {
-    /** @var int $score  */
-    private $score = 0;
+    /** @var int[] $rolls  */
+    private $rolls = array();
+    /** @var int $currentRoll */
+    private $currentRoll = 0;
 
     /**
      * Game constructor.
@@ -18,7 +20,7 @@ class Game
      */
     public function roll($pins)
     {
-        $this->score += $pins;
+        $this->rolls[$this->currentRoll++] = $pins;
     }
 
     /**
@@ -26,6 +28,74 @@ class Game
      */
     public function score()
     {
-        return $this->score;
+        $score = 0;
+        $frameIndex = 0;
+        for ($frame = 0; $frame < 10; $frame++) {
+            if ($this->isStrike($frameIndex) == 10) {
+                $score += 10 + $this->strikeBonus($frameIndex);
+                $frameIndex++;
+            } elseif ($this->isSpare($frameIndex)) {
+                $score += 10 + $this->spareBonus($frameIndex);
+                $frameIndex += 2;
+            } else {
+                $score += $this->sumOfBallsInFrame($frameIndex);
+                $frameIndex += 2;
+            }
+        }
+
+        return $score;
+    }
+
+    /**
+     * @param int $frameIndex
+     *
+     * @return int
+     */
+    private function sumOfBallsInFrame($frameIndex) {
+        return $this->rolls[$frameIndex] + $this->rolls[$frameIndex+1];
+    }
+
+    /**
+     * @param int $frameIndex
+     *
+     * @return bool
+     */
+    private function isSpare($frameIndex)
+    {
+        return ($this->sumOfBallsInFrame($frameIndex) == 10);
+    }
+
+    /**
+     * @param int $frameIndex
+     *
+     * @return bool
+     */
+    private function isStrike($frameIndex)
+    {
+        return ($this->rolls[$frameIndex] == 10);
+    }
+
+    /**
+     * Get the spare bonus in the frame.
+     *
+     * @param int $frameIndex
+     *
+     * @return int
+     */
+    private function spareBonus($frameIndex)
+    {
+        return $this->rolls[$frameIndex+2];
+    }
+
+    /**
+     * Get the strike bonus in the frame.
+     *
+     * @param int $frameIndex
+     *
+     * @return int
+     */
+    private function strikeBonus($frameIndex)
+    {
+        return $this->rolls[$frameIndex+1] + $this->rolls[$frameIndex+2];
     }
 }
--- tests/GameTest.php
+++ tests/GameTest.php
@@ -19,11 +19,22 @@ class GameTest extends \PHPUnit_Framework_TestCase
      * @param int $rolls
      * @param int $pins
      */
-    protected function rollMany($rolls, $pins)
+    private function rollMany($rolls, $pins)
     {
         for ($i = 0; $i < $rolls; $i++) {
             $this->game->roll($pins);
         }
+    }
+
+    private function rollStrike()
+    {
+        $this->game->roll(10);
+    }
+
+    private function rollSpare()
+    {
+        $this->game->roll(5);
+        $this->game->roll(5);
     }
 
     public function testGameGutterGame()
@@ -36,5 +47,30 @@ class GameTest extends \PHPUnit_Framework_TestCase
     {
         $this->rollMany(20, 1);
         $this->assertSame(20, $this->game->score());
+    }
+
+    public function testOneSpare()
+    {
+        $this->rollSpare();
+
+        $this->game->roll(3);
+        $this->rollMany(17, 0);
+        $this->assertSame(16, $this->game->score());
+    }
+
+    public function testOneStrike()
+    {
+        $this->rollStrike();
+        $this->game->roll(3);
+        $this->game->roll(4);
+
+        $this->rollMany(17, 0);
+        $this->assertSame(24, $this->game->score());
+    }
+
+    public function testPerfectGame()
+    {
+        $this->rollMany(12, 10);
+        $this->assertSame(300, $this->game->score());
     }
 }
